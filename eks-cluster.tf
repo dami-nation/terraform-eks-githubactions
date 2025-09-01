398a25f0a52ad8ecb3609f464bf60d7765ec1f65
Damilola Ola <damilolaola@MacBook-Pro.lan>
2025-09-01 14:37:42 -0500

    Clean up: remove .terraform from repo, add .gitignore

---
+++ eks-cluster.tf
@@ -0,0 +1,30 @@
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "app-eks-cluster"
+  cluster_version = "1.32"
+
+  cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+
+eks_managed_node_groups = {
+    eks-worker-nodes = {
+      instance_types = ["t3.large"]
+      min_size       = 1
+      max_size       = 5
+      desired_size   = 2
+    }
+  }
+
+  vpc_id     = module.myapp-vpc.vpc_id
+  subnet_ids = module.myapp-vpc.private_subnets
+
+  tags = {
+    Name        = "app-eks-cluster"
+    Environment = "development"
+    Terraform   = "true"
+  }
+
+}
---
+++ vpc.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  description = "AWS region where resources will be created"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "myapp-vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name            = "myapp-vpc"
+  cidr            = var.vpc_cidr_block
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
+  azs             = data.aws_availability_zones.available.names
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/app-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/app-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/app-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"       = 1
+  }
+}
